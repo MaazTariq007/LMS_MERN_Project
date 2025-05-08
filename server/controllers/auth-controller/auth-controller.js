171239e27f6bf869ba04719551398d0ccf311ba8
ali <ali@gmail.com>
2025-05-08 15:05:00 +0500

    login/register logic

--- server/controllers/auth-controller/auth-controller.js
+++ server/controllers/auth-controller/auth-controller.js
@@ -1,14 +1,89 @@
-const register = (request, response) => {
+import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const register = async (request, response) => {
   try {
+    const { userName, userEmail, userPassword, role } = request.body;
+
+    if (!userName || !userEmail || !userPassword || !role) {
+      response.status(200).json({
+        status: false,
+        msg: "All fields are required",
+      });
+    }
+
+    const alreadyExistUser = await User.findOne({
+      $or: [{ userName: userName }, { userEmail: userEmail }],
+    });
+
+    if (alreadyExistUser) {
+      response.status(200).json({
+        status: false,
+        msg: "User already exist",
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(userPassword, 10);
+
+    const newUserCreated = await User.create({
+      userName,
+      userEmail,
+      userPassword: hashPassword,
+      role,
+    });
+
+    return response.status(200).json({
+      status: true,
+      msg: "Account Created Successfully",
+      userId: newUserCreated?._id,
+    });
   } catch (error) {
-    console.log(error.message);
+    response.status(200).json({
+      success: false,
+      msg: error.message,
+    });
   }
 };
-const login = (request, response) => {
+
+// =====================================
+
+const login = async (request, response) => {
   try {
-  } catch (error) {
-    console.log(error.message);
-  }
+    const { userEmail, userPassword } = request.body;
+
+    if (!userEmail || !userPassword) {
+      return response.status(200).json({
+        success: false,
+        msg: "All Fields Are Required",
+      });
+    }
+
+    const checkUser = await User.findOne({ userEmail: userEmail });
+
+    if (!checkUser) {
+      return response.status(200).json({
+        success: false,
+        msg: "Account Does Not Exists",
+      });
+    }
+    if (await bcrypt.compare(userPassword, checkUser.userPassword)) {
+      const accessToken = jwt.sign(
+        {
+          _id: checkUser?._id,
+          userName: checkUser?.username,
+          userEmail: checkUser?.userEmail,
+          role: checkUser?.role,
+        },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "120m",
+        }
+      );
+    }
+  } catch (error) {}
 };
 
 export { register, login };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRouter from "./routes/auth-routes.js";
+import databaseConnection from "./database/connectToDb.js";
 
 dotenv.config();
 const port = process.env.PORT || 3001;
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+databaseConnection();
 app.use("/api/v1/lms", authRouter);
 
 app.listen(port, () => {
